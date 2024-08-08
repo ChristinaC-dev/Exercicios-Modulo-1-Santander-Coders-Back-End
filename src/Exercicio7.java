4185ec53720223e6293354691b93417c87e62a55
ChristinaC-dev <larissacpcsousa@gmail.com>
2024-08-07 21:47:22 -0300

    Add files via upload
    Exercício 7

---
+++ src/Exercicio7.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class Exercicio7 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o número de linhas: ");
+        int linhas = scanner.nextInt();
+
+        System.out.print("Digite o número de colunas: ");
+        int colunas = scanner.nextInt();
+
+        int[][] matriz = new int[linhas][colunas];
+
+
+        System.out.println("Digite os valores da matriz:");
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                System.out.printf("Elemento [%d][%d]: ", i, j);
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+
+        imprimirMatriz(matriz);
+
+
+        int maiorNumero = matriz[0][0];
+
+        for (int i = 0; i < linhas; i++) {
+            for (int j = 0; j < colunas; j++) {
+                if (matriz[i][j] > maiorNumero) {
+                        maiorNumero = matriz[i][j];
+                }
+            }
+        }
+
+        System.out.println("O maior elemento da matriz é: " + maiorNumero);
+
+        scanner.close();
+    }
+
+    public static void imprimirMatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
+    }
+}
