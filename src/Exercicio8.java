b4f780f3fd32bf0e52bd70792b59d287d1bba820
ChristinaC-dev <larissacpcsousa@gmail.com>
2024-08-14 07:04:13 -0300

    exercicios novos

--- src/Exercicio8.java
+++ src/Exercicio8.java
@@ -1,2 +1,44 @@
-package PACKAGE_NAME;public class Exercicio8 {
+import java.util.Scanner;
+
+public class Exercicio8 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int[][] matriz = new int[3][3];
+
+        System.out.println("Digite os valores da matriz 3x3:");
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("Elemento [%d][%d]: ", i, j);
+                matriz[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("A matriz inserida é:");
+        imprimirMatriz(matriz);
+
+        int somaDiagonalPrincipal = 0;
+        System.out.print("Os valores da diagonal principal são: ");
+        for (int i = 0; i < 3; i++) {
+            int valorDiagonal = matriz[i][i];
+            System.out.print(valorDiagonal + " ");
+            somaDiagonalPrincipal += valorDiagonal;
+        }
+
+        System.out.println();
+        System.out.println("A soma é: " + somaDiagonalPrincipal);
+
+        scanner.close();
+    }
+
+    public static void imprimirMatriz(int[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                System.out.print(matriz[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 }
--- src/Exercicio9.java
+++ src/Exercicio9.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class Exercicio9 {
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Exercicio9 {
+
+   public static void main(String[] args) {
+
+       String caminhoDoArquivo = "caminho/do/seu/arquivo.txt";
+
+       
+       BufferedReader leitor = null;
+
+       try {
+
+           leitor = new BufferedReader(new FileReader(caminhoDoArquivo));
+
+
+           String linha;
+           while ((linha = leitor.readLine()) != null) {
+               System.out.println(linha);
+           }
+       } catch (IOException e) {
+
+           System.err.println("Erro ao ler o arquivo: " + e.getMessage());
+       } finally {
+
+           if (leitor != null) {
+               try {
+                   leitor.close();
+               } catch (IOException e) {
+                   System.err.println("Erro ao fechar o arquivo: " + e.getMessage());
+               }
+           }
+       }
+   }
 }
